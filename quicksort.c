1d5e39c8211d5daf9a1d129e9cd4f0e21fbd5f93
aaronphilips <aphilips@ualberta.ca>
2016-11-30 02:22:52 -0700

    kill me

--- quicksort.c
+++ quicksort.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
-
+//literally copy pasted...
+void swap(int array[], int firstIndex,int secondIndex);
+void quickSort(int a[], int begin, int end);
 int main( int argc, char *argv[] ) {
-        printf( "argv[0]:  %s\n", argv[0] );
+        int arr[6] ={3,2,1,4,5,6};
+        quickSort(arr,0,5);
+        
+        int i;
+        for (i=0;i < sizeof (arr)/sizeof(int) ;i++) {
+        printf("%d\n",arr[i]);
+        }
 
         return 0;
+}
+
+int inPlacePartitioning(int A[], int begin, int end){
+        int pivotIndex= begin + end/2;
+        int pivotValue=A[pivotIndex];
+        while(begin <= end){
+                if(A[begin] >= pivotValue && A[end] < pivotValue){
+                swap(A,begin,end);
+                }
+                if(A[begin] < pivotValue) begin++;
+                if(A[end] >= pivotValue) end--;
+        }
+        return begin;
+}
+
+void swap(int array[], int firstIndex,int secondIndex){
+        int tmp= array[firstIndex];
+        array[firstIndex]=array[secondIndex];
+        array[secondIndex]=tmp;
+}
+
+void quickSort(int A[], int begin, int end){
+        //You may want to swap (A[pivotIndex], A[end]) and
+        //start from end-1, then return it to its place.
+       
+        int pivot = inPlacePartitioning(A, begin, end);
+        quickSort(A, begin, pivot-1);
+        quickSort(A, pivot+1, end);
 }
